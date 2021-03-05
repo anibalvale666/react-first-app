beed1a3c18f60cb65c5b6d7916c563623494632b
anibalvale666 <anibal.valenzuela@ucsp.edu.pe>
2021-03-05 13:49:19 -0500

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,27 @@
+// todos los hooks empiezan con use
+// el fecht es por convencion
+
+import { useState, useEffect } from "react"
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = ( category ) => {
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(() => {
+        
+        getGifs(category)
+            .then( imgs => {
+ 
+                setState( {
+                    data: imgs,
+                    loading: false
+                });
+ 
+            })
+    }, [category])
+
+    return state;
+}
